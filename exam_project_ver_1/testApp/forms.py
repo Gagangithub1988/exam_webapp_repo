e3dc9e6c4aca8535b53126a861afd32286bf3b52
Gagangithub1988 <65104778+Gagangithub1988@users.noreply.github.com>
2020-06-22 19:34:58 +0530

    email_validation_done_for_passwordreset

--- exam_project_ver_1/testApp/forms.py
+++ exam_project_ver_1/testApp/forms.py
@@ -1,6 +1,16 @@
 from django import forms
 from .models import Account
+from django.contrib.auth.forms import PasswordResetForm
 class AccountForm(forms.ModelForm):
     class Meta:
         model=Account
         fields=['first_name','last_name','email','contact_number','profile_pic']
+        
+class EmailValidationOnForgotPassword(PasswordResetForm):
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if not Account.objects.filter(email__iexact=email, is_active=True).exists():
+            msg = ("There is no user registered with the specified E-Mail address.")
+            self.add_error('email', msg)
+        return email
